504ac41dad05b02ea9aa09f3d7a51320c54466db
Chathuri Imesha <imeshachathuri6@gmail.com>
2024-02-26 10:26:38 +0530

    updated js part

--- weather-project/src/App.jsx
+++ weather-project/src/App.jsx
@@ -1,5 +1,4 @@
-import { useState } from 'react'
-
+import { useState,useEffect } from 'react'
 import './App.css'
 
 function App() {
@@ -8,10 +7,39 @@ function App() {
     region: 'Region',
     country: 'Country',
     data_Location: 'Date & Location',
-    temp: 'Temperature',
+    temp: 'Temp',
     cloudy: 'Cloudy',
     img:''
   });
+
+  useEffect(()=>{
+    const searchBtn = document.getElementById('searchbtn');
+    searchBtn.addEventListener('click', () => {
+      const searchVal = document.getElementById('searchtxt').value;
+      const requestOptions = {
+        method: 'GET'
+      };
+
+      console.log('Search value:',searchVal);
+
+      
+      fetch(`http://api.weatherapi.com/v1/current.json?key=7d654de21f8c45d4a6933257242502&q=${searchVal}`, requestOptions)
+        .then(response => response.json())
+        .then(data => {
+          setWeather({
+            name: data.location.name,
+            region: data.location.region,
+            country: data.location.country,
+            data_Location: data.location.localtime,
+            temp: data.current.temp_c + 'C',
+            cloudy: data.current.condition.text,
+            img: data.current.condition.icon
+          });
+        })
+        .catch(error => console.log('error', error));
+    });
+  }, []);
+
 
  
   return (
@@ -19,8 +47,8 @@ function App() {
       <div>
         <header>
           <nav>
-            <div class="nav-links" id="navLnks">
-              <i class="fa fa-times" onclick="hideMenu()"></i>
+            <div className="nav-links" id="navLnks">
+              {/* <i className="fa fa-times" onClick={hideMenu}></i> */}
               <ul>
                 <li><a href="">HOME</a></li>
                 <li><a href="#current-weather">CURRNENT</a></li>
@@ -28,11 +56,11 @@ function App() {
 
               </ul>
             </div>
-            <i class="fa fa-bars" onclick="showMenu()"></i>
+            {/* <i className="fa fa-bars" onClick={showMenu}></i> */}
           </nav>
-          <h1 class="title">🌍<b>Weather</b> Forecast</h1>
+          <h1 className="title">🌍<b>Weather</b> Forecast</h1>
 
-          <div class="user-interface">
+          <div className="user-interface">
             <input type="text" id="searchtxt" placeholder="Enter location" />
             <button id="searchbtn">Search</button>
 
@@ -41,16 +69,16 @@ function App() {
         </header>
 
         <section id="current-weather">
-          <h1 class="heders">Current Weather🍃</h1>
+          <h1 className="heders">Current Weather🍃</h1>
           <div>
 
-            <h1 id="name">Name</h1>
-            <h1 id="region">Region</h1>
-            <h1 id="country">Country</h1>
-            <h1 id="time">Date & Localtime</h1>
-            <h1 id="temp">Temp</h1>
-            <h1 id="cloudy">Cloudy</h1>
-            <img id="img" alt="" />
+            <h1 id="name">{whether.name}</h1>
+            <h1 id="region">{whether.region}</h1>
+            <h1 id="country">{whether.country}</h1>
+            <h1 id="time">{whether.data_Location}</h1>
+            <h1 id="temp">{whether.temp}</h1>
+            <h1 id="cloudy">{whether.cloudy}</h1>
+            <img id="img" src={whether.img} alt="" />
 
 
           </div>
